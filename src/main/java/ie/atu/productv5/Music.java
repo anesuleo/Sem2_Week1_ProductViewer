cc1726a1352e4595c1258a4c4c32482c69c2ca55
anesu <g00425238@atu.ie>
2024-02-05 19:05:52 +0000

    Created new project

---
+++ src/main/java/ie/atu/productv5/Music.java
@@ -0,0 +1,34 @@
+package ie.atu.productv5;
+
+import ie.atu.productv3.Product;
+
+public class Music extends Product {
+    private String artist;
+    private String label;
+
+    public Music() {
+        super();
+        artist = "";
+        label = "";
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    @Override
+    public String toString(){return super.toString() + " by " + artist + "Part of the " + label;}
+
+}
