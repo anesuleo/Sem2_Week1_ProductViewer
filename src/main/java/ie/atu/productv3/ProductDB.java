6359aab8fa70e1109b32c0a46c42e9fce84f5d75
anesu <g00425238@atu.ie>
2024-02-05 19:27:33 +0000

    Created TV and Music class

--- src/main/java/ie/atu/productv3/ProductDB.java
+++ src/main/java/ie/atu/productv3/ProductDB.java
@@ -1,12 +1,12 @@
 package ie.atu.productv3;
 
+import ie.atu.productv2.Book;
+import ie.atu.productv2.Software;
+
 public class ProductDB {
 
-    //  public static Book or Software getProduct(String productCode) {
-    // In a more realistic application, this code would get the data for the product from a file or database
-    // For now, this code just uses if/else statements to return the correct product data
-
-    //We need all the book and software objects, but what kind of object do we return?
-    //}
+     public static Product getProduct(String productCode) {
+        return Product;
+     }
 }
 
